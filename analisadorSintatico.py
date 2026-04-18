70b2d465dc2b9cf97d4fbd31321d9ee559eaaf96
Eduardo Melo <140762724+edufmelo@users.noreply.github.com>
2026-04-17 22:20:37 -0300

    feat: adicao de esqueleto para analisador sintatico

---
+++ analisadorSintatico.py
@@ -0,0 +1,30 @@
+"""
+Integrantes do grupo (ordem alfabética):
+- Daniel de Almeida Santos Bina - @danielbina
+- Eduardo Ferreira de Melo - @edufmelo
+- João Eduardo Faccin Leineker - @joaooleineker
+
+- Nome do grupo no Canvas: RA2 7
+"""
+import sys
+
+
+def lerTokens():
+    pass
+
+def construirGramatica():
+    pass
+
+def parsear():
+    pass
+
+def gerarArvore():
+    pass
+
+def main():
+    if len(sys.argv) < 2:
+        print("Uso: python analisadorSintatico.py <arquivo_teste>")
+        return
+
+if __name__ == "__main__":
+    main()
